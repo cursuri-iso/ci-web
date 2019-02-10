374c6db0b5dcf7c8aa7d3334c32dfd8472faeb9e
Mihai Osvat <mihai-osvat>
2019-02-10 11:22:21 +0200

    Added clarity

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ClarityModule } from '@clr/angular';
 
 import { AppComponent } from './app.component';
 import { NxModule } from '@nrwl/nx';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NxModule.forRoot()],
+  imports: [BrowserModule, NxModule.forRoot(), ClarityModule],
   providers: [],
   bootstrap: [AppComponent]
 })
