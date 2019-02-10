202cf5657cbbb823670e59173ee137959a9344b1
mihai-osvat <mihai-osvat>
2019-02-10 19:59:55 +0200

    fixed

--- libs/domain-lib/src/lib/services/domain-static.service.ts
+++ libs/domain-lib/src/lib/services/domain-static.service.ts
@@ -6,10 +6,10 @@ import { Injectable } from '@angular/core';
 export class DomainStaticService implements EntityService {
     constructor(){}
     getEntities(): Observable<EntityModel[]>  {
-        const domains: any[] = [];
+        const domains: EntityModel[] = [];
 
-        domains.push({ name: 'First domain', specific: 'Specific for the first domain' });
-        domains.push({ name: 'Second domain', specific: 'Specific for the second domain' });
+        domains.push(<EntityModel> { name: 'First domain', specific: 'Specific for the first domain' });
+        domains.push(<EntityModel> { name: 'Second domain', specific: 'Specific for the second domain' });
 
         return of(domains);
     }
